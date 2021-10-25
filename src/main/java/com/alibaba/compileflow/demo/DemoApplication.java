e174d502c6c998f3c52ea0234ca02a77842f50f3
xuan <xuan.xa@alibaba-inc.com>
2021-10-25 10:33:18 +0800

    bugfix

--- src/main/java/com/alibaba/compileflow/demo/DemoApplication.java
+++ src/main/java/com/alibaba/compileflow/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.alibaba.compileflow.demo;
 
+import com.alibaba.compileflow.engine.process.preruntime.generator.bean.SpringApplicationContextProvider;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author xuan
@@ -13,6 +16,11 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("DemoApplication start ok!!!");
+    }
+
+    @Bean
+    public SpringApplicationContextProvider getSpringApplicationContextProvider() {
+        return new SpringApplicationContextProvider();
     }
 
 }
